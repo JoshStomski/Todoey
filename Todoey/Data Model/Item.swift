c5556d6ceff6faac29b5b41024fdeb86aab57413
Josh Stomski <josh.stomski@yahoo.com>
2018-09-10 15:28:10 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Josh Stomski on 9/10/18.
+//  Copyright © 2018 Josh Stomski. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
